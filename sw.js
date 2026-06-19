020c37b22fd8ef180dfae070037041d8724b8418
KivikaGloria <indeokivika@gmail.com>
2026-06-19 02:57:32 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'nzoku-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './logo-192.png',
+  './logo-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
